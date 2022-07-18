0318ff34a10bab3a2e60cc171e58415019febe2e
Henry <henrywlam+github@gmail.com>
2022-07-18 19:56:14 -0400

    completed exercise

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { choice, remove } from "./helpers";
+import foods from "./foods";
+
+const randFruit = choice(foods);
+
+console.log(`I'd like one ${randFruit}, please.`)
+console.log(`Here you go: ${randFruit}`)
+console.log(`Delicious! May I have another?`)
+
+const fruitsLeft = remove(foods,randFruit);
+
+console.log(`I'm sorry, we're all out. We have ${fruitsLeft.length} left.`);
